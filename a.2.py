8b0a6a71d3c331cdff6d619247b19cfd3a1a5bce
usersaiyam <saiyamt08@gmail.com>
2025-07-23 11:53:20 +0530

    Add files via upload

---
+++ a.2.py
@@ -0,0 +1,29 @@
+'''
+num = int(input("Enter an integer: "))
+
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
+'''
+
+
+'''
+num = int(input("Enter an integer: "))
+
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
+'''
+
+'''
+total_sum = 0
+
+for number in range(1, 50):
+    total_sum += number
+
+
+print("The sum of numbers from 1 to 49 is:", total_sum)
+
+'''
